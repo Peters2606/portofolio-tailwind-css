ea8e82c5966069f7ea4deff0ff2d551ae7deed63
Peters2606 <petersgabriel704@gmail.com>
2024-08-05 13:22:59 +0700

    update dark mode dan fitur baru

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const toTop = document.querySelector('#back-to-top');
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         header.classList.remove('navbar-fixed');
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 };
 
@@ -17,3 +22,33 @@ hamburger.addEventListener('click', function () {
     navMenu.classList.toggle('hidden');
 
 })
+
+//klik diluar hamburger
+window.addEventListener('click', function (e) { 
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//dark mode toggle
+const darktoggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darktoggle.addEventListener('click',function(){
+    if(darktoggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+//membuat toggle sesuai mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+    darktoggle.checked = true;
+  } else {
+    darktoggle.checked = false;
+  }
